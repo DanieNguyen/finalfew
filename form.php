42ced2694e3c9c17fe6cb5ce0ddfe0ae4d8922a4
unknown <danngu@uw.edu>
2019-08-11 21:59:40 -0500

    why dun it love me

--- form.php
+++ form.php
@@ -2,17 +2,10 @@
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-
-    $email_from = 'danngu@uw.edu';
-    $email_sub = 'New Form Submission';
-    $email_body = 'Name: $name \n Email: $email \n Message: $message \n';
-
-    $to = 'danngu@uw.edu';
-
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $email \r\n";
-
-    mail($to, $email_sub, $email_body, $headers);
-
-    header("Location: contact.html");
+    $formcontent=" From: $name \n Message: $message";
+    $recipient = "danngu@uw.edu";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
 ?>
