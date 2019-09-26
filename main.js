71fd1f4a86dcd4fb1b7951f46c19210124007fac
Daniel Nguyen <danngu@uw.edu>
2019-09-25 22:23:00 -0700

    change mobile landing pictures for portfolio pieces

--- main.js
+++ main.js
@@ -28,9 +28,19 @@ $(document).ready(function () {
         $navBar.removeClass('fixed');
       }
     }
-
   });
 
+  var img = $('.landingpic').attr('src');
+  console.log(img);
+  var mq = window.matchMedia("(min-width:700px)")
+  if (mq.matches) {
+    var newImg = img + ".jpg";
+    $('.landingpic').attr('src', newImg);
+  } else {
+    var newImg = img + "-sm.jpg";
+    $('.landingpic').attr('src', newImg);
+  }
+  
   $.getScript("node_modules/autosize/dist/autosize.js", function () {
     autosize(document.querySelector('textarea'));
   });
